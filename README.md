242aa54777642046aa2418f58b3616b4fb9b5fab
Kyle L. Jensen <kljensen@gmail.com>
2016-01-26 11:14:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# SQL Activity for CPSC113
+
+Prior to class you read about relational databases and how they store structured
+data to enable sophisticated querying. The readings are on the
+[class website](https://cpsc113.som.yale.edu/lectures/relational-databases).
+
+In this activity, we will practice the creation of database schemas, and querying
+of data. We'll use some movie reviews based on [this activity from
+Stanford](https://lagunita.stanford.edu/courses/DB/SQL/SelfPaced/courseware/ch-sql/seq-exercise-sql_movie_mod/).
+
+## How to complete this activity
+
+### Step 0
+
+Find a partner or two in class.
+
+### Step 1
+
+Clone this repository to either your computer or [Cloud9](http://c9.io) or similar
+platform where you can use a shell and [sqlite](https://www.sqlite.org/). This is
+pre-installed on Cloud9. If you're on a mac, you likely also have it pre-installed.
+(You can type `which sqlite3` to see if you do.)
+
+We are using sqlite instead of other RDBMS such as
+[PostgreSQL](http://www.postgresql.org/) or [MySQL](https://www.mysql.com/)
+because it is easy to set up.
+
+To clone this repo, you'll likely use this command
+
+  git clone https://git.yale.edu/cpsc-113-spring-2016/sql-activity
+
+### Step 2
+
+Create a file called `class-members.txt` and populate it with the netids
+of the persons in your group. Mine would look like this
+
+  klj39
+  foo42
+  bar95
+
+Add that file to your repo with `git add class-members.txt` and commit
+it with `git commit`.
+
+### Step 3
+
+Look at the `rating.sql` file. Therein lay some SQL statements around which
+we've placed code comments. You'll be filling in missing code in there everywhere
+you see `TASK _:`. For example, the first task is to complete the schema of the
+database.
+
+### Step 4
+
+Complete the tasks. Each time you alter the code, you can run it like
+
+  sqlite3 -echo my-movie-database.db <rating.sql
+
+The `<` carrat feels the `rating.sql` file into the STDIN file descriptor
+for the sqlite3 program. It is a common way to get data into a program that
+you would normally type into. The `-echo` option to sqlite will cause the
+program to output the result of all your commands.
+
+### Step 5
+
+After you complete each task, make a git commit with a descriptive message.
+
+### Step 6
+
+After you complete all the tasks, or we stop the activity, push your repo
+up to [git.yale.edu](http://git.yale.edu) in a new public repo. We'll be
+able to see it. This will only be graded for participation.
---
+++ rating.sql
@@ -0,0 +1,68 @@
+-- Delete the tables if they already exist
+drop table if exists Movie;
+drop table if exists Reviewer;
+drop table if exists Rating;
+
+-- Create the schema for our tables.
+-- TASK A: complete the schema. TWe did the Reviewer.
+create table Movie(mID int, title text, year int, director text);
+create table Reviewer( /* COMPLETE ME */ );
+create table Rating( /* COMPLETE ME */ );
+
+
+-- Movie data.
+-- The columns are the id, title, year of release, and director
+insert into Movie values(101, 'Gone with the Wind', 1939, 'Victor Fleming');
+insert into Movie values(102, 'Star Wars', 1977, 'George Lucas');
+insert into Movie values(103, 'The Sound of Music', 1965, 'Robert Wise');
+insert into Movie values(104, 'E.T.', 1982, 'Steven Spielberg');
+insert into Movie values(105, 'Titanic', 1997, 'James Cameron');
+insert into Movie values(106, 'Snow White', 1937, null);
+insert into Movie values(107, 'Avatar', 2009, 'James Cameron');
+insert into Movie values(108, 'Raiders of the Lost Ark', 1981, 'Steven Spielberg');
+
+-- Reviewer data.
+-- The columns are the id and name of the reviewer.
+insert into Reviewer values(201, 'Sarah Martinez');
+insert into Reviewer values(202, 'Daniel Lewis');
+insert into Reviewer values(203, 'Brittany Harris');
+insert into Reviewer values(204, 'Mike Anderson');
+insert into Reviewer values(205, 'Chris Jackson');
+insert into Reviewer values(206, 'Elizabeth Thomas');
+insert into Reviewer values(207, 'James Cameron');
+insert into Reviewer values(208, 'Ashley White');
+-- TASK B: Add yourself to these data
+
+-- Rating data.
+-- The columns are the id of the user, id of the movie, rating, and date of rating.
+insert into Rating values(201, 101, 2, '2011-01-22');
+insert into Rating values(201, 101, 4, '2011-01-27');
+insert into Rating values(202, 106, 4, null);
+insert into Rating values(203, 103, 2, '2011-01-20');
+insert into Rating values(203, 108, 4, '2011-01-12');
+insert into Rating values(203, 108, 2, '2011-01-30');
+insert into Rating values(204, 101, 3, '2011-01-09');
+insert into Rating values(205, 103, 3, '2011-01-27');
+insert into Rating values(205, 104, 2, '2011-01-22');
+insert into Rating values(205, 108, 4, null);
+insert into Rating values(206, 107, 3, '2011-01-15');
+insert into Rating values(206, 106, 5, '2011-01-19');
+insert into Rating values(207, 107, 5, '2011-01-20');
+insert into Rating values(208, 104, 3, '2011-01-02');
+-- TASK C: Add yourself to these data
+
+
+-- TASK D: Write a query that shows how many movies there are.
+--         Hint: use COUNT
+
+-- TASK E: Write a query that shows which three people submitted the most reviews.
+--         Hint: use ORDER BY, and LIMIT
+
+-- TASK F: Write a query that shows the top 3 rated movies
+--         Hint: use LEFT JOIN, ORDER BY, and LIMIT
+
+-- TASK G: Write a query that shows how many ratings each move received
+--         Hint: use LEFT JOIN, GROUP BY, and COUNT
+
+-- TASK H: Write a query that shows which user gives the lowest average rating
+--         Hint: use JOIN, GROUP BY, and AVERAGE
